df1de5d1bbfb7aac11383f61f57ca21c17038217
RDSMatheus <matheus_rds1998@hotmail.com>
2024-11-25 20:06:21 -0300

    implementação do celebrate no error-handler

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
 import { getFirestore } from 'firebase-admin/firestore';
-import { ValidationError } from '../errors/validation.error';
 import { NotFoundError } from '../errors/not-found.error';
 
 type User = {
@@ -37,9 +36,7 @@ export class UserController {
 
   static async save(req: Request, res: Response, next: NextFunction) {
     let user = req.body;
-    if (!user.email || user.email.length === 0) {
-      throw new ValidationError('E-mail obrigatório!');
-    }
+    
     await getFirestore().collection('users').add(user);
     res.status(201).send({
       message: 'Usuário criado com sucesso!',
