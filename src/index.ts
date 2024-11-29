fb7dfb74c3159ebbb102eb9219c17558142bc698
RDSMatheus <matheus_rds1998@hotmail.com>
2024-11-28 22:14:27 -0300

    auth controller e auth login schema criado

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import { initializeApp } from 'firebase-admin/app';
+import { initializeApp as initializeFirebaseApp } from 'firebase/app'
 import { routes } from './routes';
 import { errorHandler } from './middlewares/error-handler.middleware';
 import { pageNotFoundHandler } from './middlewares/page-not-found.middleware';
 
 initializeApp();
+initializeFirebaseApp({
+  apiKey: process.env.API_KEY
+})
 const app = express();
 
 app.get('/', (req, res) => {
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { userRoutes } from './users.routes';
-
+import { authRoutes } from './auth.routes';
 
 export const routes = (app: express.Express) => {
   app.use(express.json());
+  app.use(authRoutes);
   app.use(userRoutes);
 };
